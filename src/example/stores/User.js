3bcecc5c8627911e0cf322b6cc67024c9aece26e
a <sonayen.a@gmail.com>
2018-06-10 19:11:51 +0300

    Add user store

---
+++ src/example/stores/User.js
@@ -0,0 +1,10 @@
+import ContextStore from '../../lib';
+
+export default class UserStore extends ContextStore {
+  state = {
+    name: 'Alice',
+    signedIn: false
+  };
+
+  signIn = () => this.setState({ signedIn: true });
+}
