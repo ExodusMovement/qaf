f730d77ef2249bddf4b1a1a83dce84ae3859e4f7
a <sonayen.a@gmail.com>
2018-06-20 00:40:07 +0300

    Use parcel

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import { render } from 'react-dom'; // eslint-disable-line import/no-extraneous-dependencies
-
-import Example from './example';
-
-render(<Example />, document.getElementById('root'));
