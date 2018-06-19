dc756c61df947ba4601b636c8fff3d822546b8c3
a <sonayen.a@gmail.com>
2018-06-19 22:49:14 +0300

    Setup Subscribe and setState

--- src/lib/index.js
+++ src/lib/index.js
@@ -3,5 +3,6 @@
 import create from './create';
 import inject from './inject';
 import Provider from './Provider';
+import Subscribe from './Subscribe';
 
-export { create as default, inject, Provider };
+export { create as default, inject, Provider, Subscribe };
