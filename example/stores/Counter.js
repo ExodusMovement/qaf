8c1252789e591217fc9ddc5118c7850cef6be00b
a <sonayen.a@gmail.com>
2018-06-21 11:51:06 +0300

    Remove example

--- example/stores/Counter.js
+++
@@ -1,8 +0,0 @@
-import { createStore } from '../../src';
-
-export default class CounterStore extends createStore() {
-  state = { counter: 0 };
-
-  inc = () => this.setState(state => ({ counter: state.counter + 1 }));
-  dec = () => this.setState(state => ({ counter: state.counter - 1 }));
-}
--- example/stores/index.js
+++
@@ -1,9 +0,0 @@
-import CounterStore from './Counter';
-import TweetsStore from './Tweets';
-import UserStore from './User';
-
-export default {
-  counterStore: CounterStore,
-  tweetsStore: TweetsStore,
-  userStore: UserStore
-};
