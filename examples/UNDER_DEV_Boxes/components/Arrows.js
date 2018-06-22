39625a7572340a8e16aff21d91170585517a38ae
a <sonayen.a@gmail.com>
2018-06-22 11:30:10 +0300

    Drop extra examples for now

--- examples/UNDER_DEV_Boxes/components/Arrows.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import Arrow from './Arrow';
-
-export default ({ arrows }) => (
-  <svg>{arrows.map(arrow => <Arrow {...arrow} key={arrow.id} />)}</svg>
-);
--- examples/UNDER_DEV_Boxes/components/Boxes.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-import Box from './Box';
-
-export default ({ boxes, select, move, selected }) =>
-  Object.values(boxes).map(box => (
-    <Box {...box} {...{ select, move, selected }} key={box.id} />
-  ));
