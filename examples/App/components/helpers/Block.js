e2b8b928c4570649e0c4cbcb3d94530d5d0f7e92
a <sonayen.a@gmail.com>
2018-06-26 19:21:21 +0300

    Truncate long titles

--- examples/App/components/helpers/Block.js
+++ examples/App/components/helpers/Block.js
@@ -1,10 +1,12 @@
 import TextInput from './TextInput';
 
-const Block = TextInput.withComponent('div').extend`
-  align-items: center;
-  display: flex;
-  justify-content: ${({ small }) => small && 'center'};
+const Block = TextInput.withComponent('div').extend.attrs({ type: null })`
+  line-height: 40px;
+  overflow: hidden;
+  text-align: ${({ small }) => small && 'center'};
   text-decoration: ${({ strikethrough }) => strikethrough && 'line-through'};
+  text-overflow: ellipsis;
+  white-space: nowrap;
 `;
 
 export default Block;
