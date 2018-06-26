8510fe662afa4e6470bcedb3ed1327b9d50bec8e
a <sonayen.a@gmail.com>
2018-06-26 18:56:24 +0300

    Avoid another extra template string

--- examples/App/components/helpers/Block.js
+++ examples/App/components/helpers/Block.js
@@ -4,6 +4,7 @@ const Block = TextInput.withComponent('div').extend`
   align-items: center;
   display: flex;
   justify-content: ${({ small }) => small && 'center'};
+  text-decoration: ${({ strikethrough }) => strikethrough && 'line-through'};
 `;
 
 export default Block;
--- examples/App/components/helpers/TextInput.js
+++ examples/App/components/helpers/TextInput.js
@@ -3,11 +3,7 @@ import Button from './Button';
 const TextInput = Button.withComponent('input').extend.attrs({ type: 'text' })`
   padding-left: 20px;
   padding-right: 20px;
-
-  ${({ small, large, strikethrough }) => `
-    text-decoration: ${strikethrough && 'line-through'};
-    width: ${small ? 50 : large ? 340 : 250}}px;
-  `}
+  width: ${({ small, large }) => (small ? 50 : large ? 340 : 250)}px;
 
   &:enabled {
     cursor: text;
