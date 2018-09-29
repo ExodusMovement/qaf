e41182c87622d11097115fd16760a0a501b45fbe
Ain Sal <ain@abcd.sa>
2018-09-29 09:14:23 +0300

    Add more dev friendly error messages

--- next/src/qaf/index.js
+++ next/src/qaf/index.js
@@ -15,11 +15,23 @@ export default () => {
 
   Store.getState = () => Store.dispatch({ type: 'getState' })
 
+  // TODO: make it more stable
+  // https://stackoverflow.com/a/50019873/5470921
   Store.dispatch = ({ type, ...payload }) => {
     if (!Store.ref.current) {
       throw new Error(
         `Action \`${type}\` was fired without a valid store reference or before the store is mounted (too early).`
       )
+    }
+
+    if (!Store.ref.current[type]) {
+      throw new Error(
+        `Action \`${type}\` doesn't exist in the store. Double-check the action name.`
+      )
+    }
+
+    if (typeof Store.ref.current[type] !== 'function') {
+      throw new Error(`Action \`${type}\` is not a function.`)
     }
 
     return Store.ref.current[type](payload)
